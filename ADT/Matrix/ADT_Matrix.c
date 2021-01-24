1cef5d061c0171a80560202c8ef0ba22d8080040
JMVI <jaime7592@gmail.com>
2021-01-24 18:16:31 -0400

    Commit - 01/24/2021 * Added files: - ADT for matrices. Library file. * Modified files: - ADT_Matrix.h: New prototypes added.

--- ADT/Matrix/ADT_Matrix.c
+++ ADT/Matrix/ADT_Matrix.c
@@ -1,87 +1,50 @@
 /* -----------------------------------------------------------------------------
  * Copyright (C) 2021 Jaime M. Villegas I. <jaime7592@gmail.com>
  * -----------------------------------------------------------------------------
- * Filename      : ADT_Matrix.h
- * Description   : Abstract Data Type for matrices. Header file.
+ * Filename      : ADT_Matrix.c
+ * Description   : Abstract Data Type for matrices. Library file.
  * Version       : 01.00
- * Revision      : 01
+ * Revision      : 00
  * Last modified : 01/21/2021
  * -----------------------------------------------------------------------------
  */
-
-#ifndef _MATRIX_H_
-#define _MATRIX_H_
 
 //----------------------------------------------------------------------------//
 //                                Header files                                //
 //----------------------------------------------------------------------------//
 
-#include<stdint.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-
-//----------------------------------------------------------------------------//
-//                                  Macros                                    //
-//----------------------------------------------------------------------------//
-
-// Bool data type
-#define TRUE      (uint8_t)(1)
-#define FALSE     (uint8_t)(0)
-
-// Max. dimension
-#define MAX_DIM (uint8_t)(100)
+#include"ADT_Matrix.h"
 
 //----------------------------------------------------------------------------//
 //                            General definitions                             //
 //----------------------------------------------------------------------------//
 
-// Data type stored in matrix
-typedef double Data;
-
-// Vector (1-D array)
-typedef Data* Vector;
-
-// Matrix
-typedef struct matrix_struct
+// Vector handler
+t_MatrixHandler Mx_Hdlr =
 {
-  uint8_t rows;
-  uint8_t columns;
-  double* determinant;
-  Vector* matrix;
-}
-t_matrix;
+  matrix_create,            // Create Matrix
+  matrix_identity,          // Identity Matrix
+  matrix_isNull,            // Null matrix?
+  matrix_getRows,           // Get #rows
+  matrix_getColumns,        // Get #columns
+  matrix_getDeterminant,    // Get determinant
+  matrix_update,            // Update element
+  matrix_areEqual,          // M1 == M2?
+  matrix_sum,               // Matrix sum
+  matrix_scalar,            // Scalar product
+  matrix_product,           // Matrix product
+  matrix_power,             // Matrix power
+  matrix_element_product,   // Element product
+  matrix_element_division,  // Element division
+  matrix_element_power,     // Element power
+  matrix_inverse,           // Inverse matrix
+  matrix_transpose,         // Transpose
+  matrix_minor,             // Minor ij
+  matrix_cofactor,          // Cofactor ij
+  matrix_diagonal,          // Diagonal matrix
+  matrix_delete             // Delete matrix
+};
 
-typedef t_matrix* Matrix;
-
-// Matrix handler
-typedef struct matrix_handler
-{
-  Matrix  (*init)(uint8_t Rows, uint8_t Columns);            // Create Matrix
-  Matrix  (*eye)(uint8_t Rows, uint8_t Columns);             // Identity Matrix
-  Matrix  (*isNull)(Matrix M);                               // Null matrix?
-  uint8_t (*row)(Matrix M);                                  // Get #rows
-  uint8_t (*col)(Matrix M);                                  // Get #columns
-  uint8_t (*det)(Matrix M, double* detval);                  // Get determinant
-  uint8_t (*update)(Matrix M, Data k, uint8_t i, uint8_t j); // Update element
-  uint8_t (*areEqual)(Matrix M1, Matrix M2);                 // M1 == M2?
-  Matrix  (*sum)(Matrix M1, Matrix M2);                      // Matrix sum
-  Matrix  (*scalar)(Data k, Matrix M);                       // Scalar product
-  Matrix  (*product)(Matrix M1, Matrix M2);                  // Matrix product
-  Matrix  (*pow)(Matrix M, uint8_t n);                       // Matrix power
-  Matrix  (*eProduct)(Matrix M1, Matrix M2);                 // Element product
-  Matrix  (*eDivision)(Matrix M1, Matrix M2);                // Element division
-  Matrix  (*ePow)(Matrix M, double n);                       // Element power
-  Matrix  (*inv)(Matrix M);                                  // Inverse matrix
-  Matrix  (*transp)(Matrix M);                               // Transpose
-  Matrix  (*minor)(Matrix M, uint8_t i, uint8_t j);          // Minor ij
-  uint8_t (*cof)(Matrix M, uint8_t i, uint8_t j, double* cf);// Cofactor ij
-  Matrix  (*diag)(Vector D)                                  // Diagonal matrix
-  uint8_t (*del)(Matrix M);                                  // Delete matrix
-}
-t_MatrixHandler;
-
-extern t_MatrixHandler Mx_Hdlr;
 
 //----------------------------------------------------------------------------//
 //                              Public functions                              //
@@ -93,7 +56,38 @@ extern t_MatrixHandler Mx_Hdlr;
         columns: Number of columns
 @retval Pointer to new matrix
 */
-extern Matrix matrix_create(uint8_t rows, uint8_t columns);
+Matrix matrix_create(uint8_t rows, uint8_t columns)
+{
+  Matrix M = NULL;        // New matrix
+  uint8_t i = 0, j = 0;   // Iterators
+
+  // Allocates memory for pointers array
+  M = (Matrix)calloc( rows, sizeof(Vector) );
+
+  // Returns in case of memory allocation error
+  if(M == NULL)
+  {
+    return M;
+  }
+
+for(i = 0; i < m; i++)
+   {
+      M[i] = (int*)calloc(n, sizeof(int));
+      if(M[i] == NULL)
+      {
+         printf("Memory allocation error\n");
+         exit(-1);
+      }
+      
+      for(j = 0; j < n; j++)
+      {
+         printf("M[%d,%d]: ", i + 1, j + 1); scanf("%d", &M[i][j]);
+         printf("\n");
+      }
+   }
+   
+   return M;
+}
 
 /**
 @brief  Allocates memory to create a new identity matrix.
@@ -101,28 +95,40 @@ extern Matrix matrix_create(uint8_t rows, uint8_t columns);
         columns: Number of columns
 @retval Pointer to new matrix
 */
-extern Matrix matrix_identity(uint8_t rows, uint8_t columns);
+Matrix matrix_identity(uint8_t rows, uint8_t columns)
+{
+  return NULL;
+}
 
 /**
 @brief  Verifies if matrix is null
 @param  M: Pointer to matrix
 @retval TRUE if matrix is null, FALSE otherwise
 */
-extern uint8_t matrix_isNull(Matrix M);
+uint8_t matrix_isNull(Matrix M)
+{
+  return TRUE;
+}
 
 /**
 @brief  Gets number of rows of indicated matrix
 @param  M: Pointer to matrix
 @retval Number of rows
 */
-extern uint8_t matrix_getRows(Matrix M);
+uint8_t matrix_getRows(Matrix M)
+{
+  return 0;
+}
 
 /**
 @brief  Gets number of columns of indicated matrix
 @param  M: Pointer to matrix
 @retval Number of columns
 */
-extern uint8_t matrix_getColumns(Matrix M);
+uint8_t matrix_getColumns(Matrix M)
+{
+  return 0;
+}
 
 /**
 @brief  Gets determinant of matrix if defined
@@ -130,7 +136,10 @@ extern uint8_t matrix_getColumns(Matrix M);
         detval: Determinant value
 @retval TRUE if determinant exists, FALSE otherwise
 */
-extern uint8_t matrix_getDeterminant(Matrix M, double* detval);
+uint8_t matrix_getDeterminant(Matrix M, double* detval)
+{
+  return TRUE;
+}
 
 /**
 @brief  Updates a value of introduced matrix
@@ -140,7 +149,10 @@ extern uint8_t matrix_getDeterminant(Matrix M, double* detval);
         j: Column index
 @retval TRUE if element was updated, FALSE otherwise
 */
-extern uint8_t matrix_update(Matrix M, Data k, uint8_t i, uint8_t j);
+uint8_t matrix_update(Matrix M, Data k, uint8_t i, uint8_t j)
+{
+  return TRUE;
+}
 
 /**
 @brief  Verifies if M1 and M2 are equal
@@ -148,7 +160,10 @@ extern uint8_t matrix_update(Matrix M, Data k, uint8_t i, uint8_t j);
         M2: Pointer to second matrix
 @retval TRUE if matrices are equal, FALSE otherwise
 */
-extern uint8_t matrix_areEqual(Matrix M1, Matrix M2);
+uint8_t matrix_areEqual(Matrix M1, Matrix M2)
+{
+  return 0;
+}
 
 /**
 @brief  Obtains the algebraic sum of two matrices
@@ -157,7 +172,10 @@ extern uint8_t matrix_areEqual(Matrix M1, Matrix M2);
 @retval Pointer to sum matrix
 @note   If dimensions do not match, the function returns a NULL pointer
 */
-extern Matrix matrix_sum(Matrix M1, Matrix M2);
+Matrix matrix_sum(Matrix M1, Matrix M2)
+{
+  return NULL;
+}
 
 /**
 @brief  Obtains the multiplication of a matrix and a scalar factor
@@ -165,7 +183,10 @@ extern Matrix matrix_sum(Matrix M1, Matrix M2);
         k: Scalar factor
 @retval Pointer to scalated matrix
 */
-extern Matrix matrix_scalar(Data k, Matrix M);
+Matrix matrix_scalar(Data k, Matrix M)
+{
+  return NULL;
+}
 
 /**
 @brief  Obtains the algebraic multiplication of two matrices
@@ -174,7 +195,10 @@ extern Matrix matrix_scalar(Data k, Matrix M);
 @retval Pointer to product matrix
 @note If dimensions do not match, the function returns a NULL pointer
 */
-extern Matrix matrix_product(Matrix M1, Matrix M2);
+Matrix matrix_product(Matrix M1, Matrix M2)
+{
+  return NULL;
+}
 
 /**
 @brief  Obtains the algebraic integer power of a matrix
@@ -183,7 +207,10 @@ extern Matrix matrix_product(Matrix M1, Matrix M2);
 @retval Pointer to power matrix
 @note If dimensions do not match, the function returns a NULL pointer
 */
-extern Matrix matrix_power(Matrix M, uint8_t n);
+Matrix matrix_power(Matrix M, uint8_t n)
+{
+  return NULL;
+}
 
 /**
 @brief  Obtains the element-wise multiplication of two matrices
@@ -192,7 +219,10 @@ extern Matrix matrix_power(Matrix M, uint8_t n);
 @retval Pointer to product matrix
 @note If dimensions do not match, the function returns a NULL pointer
 */
-extern Matrix matrix_element_product(Matrix M1, Matrix M2);
+Matrix matrix_element_product(Matrix M1, Matrix M2)
+{
+  return NULL;
+}
 
 /**
 @brief  Obtains the element-wise division of two matrices
@@ -201,7 +231,10 @@ extern Matrix matrix_element_product(Matrix M1, Matrix M2);
 @retval Pointer to division matrix
 @note If dimensions do not match, the function returns a NULL pointer
 */
-extern Matrix matrix_element_division(Matrix M1, Matrix M2);
+Matrix matrix_element_division(Matrix M1, Matrix M2)
+{
+  return NULL;
+}
 
 /**
 @brief  Obtains the element-wise power of a matrix
@@ -209,7 +242,10 @@ extern Matrix matrix_element_division(Matrix M1, Matrix M2);
         n: Exponent
 @retval Pointer to power matrix
 */
-extern Matrix matrix_element_power(Matrix M, double n);
+Matrix matrix_element_power(Matrix M, double n)
+{
+  return NULL;
+}
 
 /**
 @brief  Gets inverse matrix of M if existing
@@ -217,14 +253,20 @@ extern Matrix matrix_element_power(Matrix M, double n);
 @retval Pointer to inverse matrix 
 @note Returns NULL if inverse matrix is undefined
 */
-extern Matrix matrix_inverse(Matrix M);
+Matrix matrix_inverse(Matrix M)
+{
+  return NULL;
+}
 
 /**
 @brief  Transposes matrix M
 @param  M: Pointer to matrix
 @retval Pointer to transposed matrix
 */
-extern Matrix matrix_transpose(Matrix M);
+Matrix matrix_transpose(Matrix M)
+{
+  return NULL;
+}
 
 /**
 @brief  Gets minor ij of matrix M if existing
@@ -234,7 +276,10 @@ extern Matrix matrix_transpose(Matrix M);
 @retval Pointer to minor 
 @note Returns NULL if minor is undefined
 */
-extern Matrix matrix_minor(Matrix M, uint8_t i, uint8_t j);
+Matrix matrix_minor(Matrix M, uint8_t i, uint8_t j)
+{
+  return NULL;
+}
 
 /**
 @brief  Gets cofactor ij of matrix M if existing
@@ -244,7 +289,10 @@ extern Matrix matrix_minor(Matrix M, uint8_t i, uint8_t j);
         cof: Pointer to cofactor value
 @retval TRUE if cofactor exists, FALSE otherwise
 */
-extern uint8_t matrix_cofactor(Matrix M, uint8_t i, uint8_t j, double* cf);
+uint8_t matrix_cofactor(Matrix M, uint8_t i, uint8_t j, double* cf)
+{
+  return TRUE;
+}
 
 /**
 @brief  Gets minor ij of matrix M if existing
@@ -254,28 +302,28 @@ extern uint8_t matrix_cofactor(Matrix M, uint8_t i, uint8_t j, double* cf);
 @retval Pointer to minor 
 @note Returns NULL if minor is undefined
 */
-extern Matrix matrix_minor(Matrix M, uint8_t i, uint8_t j);
+Matrix matrix_minor(Matrix M, uint8_t i, uint8_t j)
+{
+  return NULL;
+}
 
 /**
 @brief  Gets a matrix with vector V as main diagonal
 @param  D: Main diagonal
 @retval Pointer to matrix 
 */
-extern Matrix matrix_diagonal(Vector D);
+Matrix matrix_diagonal(Vector D)
+{
+  return NULL;
+}
 
 /**
 @brief  Deletes matrix and frees allocated memory
 @param  M: Pointer to matrix
 @retval TRUE if matrix was deleted with no error, FALSE otherwise
 */
-extern uint8_t matrix_delete(Matrix M);
-
-/**
-@brief  Prints matrix on screen
-@param  M: Pointer to matrix
-@retval TRUE if matrix was printed with no error, FALSE otherwise
-*/
-extern uint8_t matrix_print(Matrix M);
-
-#endif
+uint8_t matrix_delete(Matrix M)
+{
+  return TRUE;
+}
 
